c60190fbc5d86a6d2545360eb1f47e64648d4f92
Akash6421 <akash.cheviti@gmail.com>
2023-08-08 13:35:24 -0400

    Complete_Application

--- script.js
+++ script.js
@@ -7,23 +7,55 @@
     const digitButtons = document.querySelectorAll('.digit');
     const clearButton = document.querySelector('.clear');
     const equalButton = document.querySelector('.equals');
+    const operatorButton = document.querySelectorAll('.operator');
+    const clickSound = document.getElementById('clickSound');
+    const clickEqual = document.getElementById('equal');
+    const clickClear = document.getElementById('clear');
+    const clickOperator = document.getElementById('operator');
 
-    clearButton.addEventListener("click", handleClear);
-    equalButton.addEventListener("click",handleEqual);
+    clearButton.addEventListener("click", () => {
+        playClearSound();
+        handleClear();
+    });
+
+    equalButton.addEventListener("click", () => {
+        playEqualSound(); 
+        handleEqual();
+    });
 
     digitButtons.forEach(button =>{
         button.addEventListener("click",() =>{
+            playClickSound();
             handleClick(button.innerText);
             updateDisplay();
         });
     });
 
+    
+    operatorButton.forEach(button => {
+        button.addEventListener("click", () => {
+            playOperatorSound();
+            handleOperator(button.innerText);
+            updateDisplay();
+        })
+    })
+
     function handleClick(digit){
-        if(displayValue === 0){
-            displayValue = digit;
+        if(operator === ''){
+            firstNumber += digit;
         }
         else{
-            displayValue += digit;
+            secondNumber += digit;
+        }
+        displayValue = (operator === '') ? firstNumber : secondNumber
+        updateDisplay();
+    }
+
+    function handleOperator(operatorselected){
+        if(firstNumber !== '' && operator === ''){
+            operator = operatorselected;
+            displayValue = operator;
+            updateDisplay();
         }
     }
 
@@ -36,12 +68,35 @@
     }
 
     function handleEqual(){
-        
-        updateDisplay();
+        if(firstNumber!== '' && secondNumber !== '' && operator !== ''){
+            const result = operate(operator, parseFloat(firstNumber), parseFloat(secondNumber));
+            displayValue = result.toString();
+            firstNumber = result.toString();
+            operator = '';
+            secondNumber = '';
+            updateDisplay();
+        }
    }
     function updateDisplay(){
         display.textContent = displayValue;
     }
+
+    function playClickSound() {
+        clickSound.currentTime = 0; // Reset the sound to the beginning
+        clickSound.play();
+      }
+      function playClearSound() {
+        clickClear.currentTime = 0; // Reset the sound to the beginning
+        clickClear.play();
+      }
+      function playEqualSound() {
+        clickEqual.currentTime = 0; // Reset the sound to the beginning
+        clickEqual.play();
+      }
+      function playOperatorSound() {
+        clickOperator.currentTime = 0; // Reset the sound to the beginning
+        clickOperator.play();
+      }
     // Function to add two numbers
     function add(a,b){
         return a+b;
@@ -59,7 +114,7 @@
     // Function to divide two numbers
     function divide(a,b){
         if(b === 0)
-        throw Error("Cannot divide by zero!");
+        alert("LOL!! Cannot divide by zero!");
         else{
         return a/b;
         }
